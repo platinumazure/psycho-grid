cba0fb4356881ab66b043fdcb2b34f8135e7dd8a
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2017-05-31 22:18:17 -0500

    Adding setPosition method to GamePiece

--- tests/lib/game-piece.js
+++ tests/lib/game-piece.js
@@ -77,4 +77,61 @@ describe("GamePiece", () => {
             assert.doesNotThrow(() => new GamePiece({ type, position }));
         });
     });
+
+    describe("setPosition", () => {
+        let gamePiece;
+
+        beforeEach(() => {
+            gamePiece = new GamePiece({ type, position });
+        });
+
+        it("should throw if position is missing", () => {
+            assert.throws(
+                () => gamePiece.setPosition(),
+                TypeError,
+                /^position is required$/
+            );
+        });
+
+        it("should throw if position row is non-numeric", () => {
+            const pos = Object.assign({}, position, { row: "foo" });
+            assert.throws(
+                () => gamePiece.setPosition(pos),
+                TypeError,
+                /^position.row must be numeric$/
+            );
+        });
+
+        it("should throw if position row is negative", () => {
+            const pos = Object.assign({}, position, { row: -1 });
+            assert.throws(
+                () => gamePiece.setPosition(pos),
+                RangeError,
+                /^position.row must be nonnegative$/
+            );
+        });
+
+        it("should throw if position column is non-numeric", () => {
+            const pos = Object.assign({}, position, { column: "foo" });
+            assert.throws(
+                () => gamePiece.setPosition(pos),
+                TypeError,
+                /^position.column must be numeric$/
+            );
+        });
+
+        it("should throw if position column is negative", () => {
+            const pos = Object.assign({}, position, { column: -1 });
+            assert.throws(
+                () => gamePiece.setPosition(pos),
+                RangeError,
+                /^position.column must be nonnegative$/
+            );
+        });
+
+        it("should set position if position is valid", () => {
+            gamePiece.setPosition({ row: 2, column: 1 });
+            assert.deepEqual(gamePiece.position, { row: 2, column: 1 });
+        });
+    });
 });
