2624f95a40b35bb4e0a0c58254823419b6e059ff
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2017-06-10 14:13:38 -0500

    Added addPiece, getPieceAt, isPositionValid methods to board
    Also added test for constructor calling addPiece on initial pieces

--- tests/lib/board.js
+++ tests/lib/board.js
@@ -61,5 +61,129 @@ describe("Board", () => {
         it("should not throw if height and width are valid", () => {
             assert.doesNotThrow(() => new Board({ height: 5, width: 7 }));
         });
+
+        describe("with valid height/width and initialPieces", () => {
+            it("should add each provided piece to the board", () => {
+                const board = new Board({
+                    height: 4,
+                    width: 4,
+                    initialPieces: [
+                        {
+                            type: {},
+                            position: { row: 0, column: 0 }
+                        },
+                        {
+                            type: {},
+                            position: { row: 3, column: 3 }
+                        }
+                    ]
+                });
+
+                assert.ok(board.getPieceAt({ row: 0, column: 0 }));
+                assert.ok(board.getPieceAt({ row: 3, column: 3 }));
+            });
+        });
+    });
+
+    describe("isPositionValid", () => {
+        let board;
+
+        beforeEach(() => {
+            board = new Board({ height: 4, width: 4 });
+        });
+
+        it("should return true if position is within board", () => {
+            assert.isTrue(board.isPositionValid({ row: 0, column: 0 }));
+            assert.isTrue(board.isPositionValid({ row: 0, column: 1 }));
+            assert.isTrue(board.isPositionValid({ row: 0, column: 3 }));
+            assert.isTrue(board.isPositionValid({ row: 1, column: 0 }));
+            assert.isTrue(board.isPositionValid({ row: 1, column: 1 }));
+            assert.isTrue(board.isPositionValid({ row: 1, column: 3 }));
+            assert.isTrue(board.isPositionValid({ row: 3, column: 0 }));
+            assert.isTrue(board.isPositionValid({ row: 3, column: 1 }));
+            assert.isTrue(board.isPositionValid({ row: 3, column: 3 }));
+        });
+
+        it("should return false if position is outside of board", () => {
+            assert.isFalse(board.isPositionValid({ row: -1, column: 0 }));
+            assert.isFalse(board.isPositionValid({ row: -1, column: -1 }));
+            assert.isFalse(board.isPositionValid({ row: 0, column: -1 }));
+            assert.isFalse(board.isPositionValid({ row: 4, column: 3 }));
+            assert.isFalse(board.isPositionValid({ row: 4, column: -1 }));
+            assert.isFalse(board.isPositionValid({ row: 3, column: 4 }));
+        });
+    });
+
+    describe("addPiece", () => {
+        let board;
+
+        beforeEach(() => {
+            board = new Board({ height: 4, width: 4 });
+        });
+
+        it("should throw RangeError if position is outside of board", () => {
+            assert.throws(
+                () => board.addPiece({ type: {}, position: { row: -1, column: 0 } }),
+                RangeError,
+                /^Row -1, column 0 is an invalid position$/
+            );
+        });
+
+        it("should add a piece successfully if position is within board", () => {
+            const piece = { type: {}, position: { row: 0, column: 0 } };
+            board.addPiece(piece);
+
+            assert.strictEqual(board.getPieceAt({ row: 0, column: 0 }), piece);
+        });
+
+        describe("with piece on the board", () => {
+            beforeEach(() => {
+                board.addPiece({ type: {}, position: { row: 0, column: 0 } });
+            });
+
+            it("should throw RangeError if piece is already present in given position", () => {
+                assert.throws(
+                    () => board.addPiece({ type: {}, position: { row: 0, column: 0 } }),
+                    RangeError,
+                    /^Piece is already present at row 0, column 0$/
+                );
+            });
+
+            it("should not throw if different location is used for new piece", () => {
+                const piece = { type: {}, position: { row: 0, column: 1 } };
+                board.addPiece(piece);
+
+                assert.strictEqual(board.getPieceAt({ row: 0, column: 1 }), piece);
+            });
+        });
+    });
+
+    describe("getPieceAt", () => {
+        const pieceOne = { type: {}, position: { row: 0, column: 0 } };
+        const pieceTwo = { type: {}, position: { row: 2, column: 2 } };
+        let board;
+
+        beforeEach(() => {
+            board = new Board({ height: 4, width: 4 });
+            board.addPiece(pieceOne);
+            board.addPiece(pieceTwo);
+        });
+
+        it("should return piece if piece is present at given position", () => {
+            assert.strictEqual(board.getPieceAt({ row: 0, column: 0 }), pieceOne);
+            assert.strictEqual(board.getPieceAt({ row: 2, column: 2 }), pieceTwo);
+        });
+
+        it("should reutrn null if no piece is present", () => {
+            assert.strictEqual(board.getPieceAt({ row: 1, column: 1 }), null);
+        });
+
+        it("should throw RangeError if position is invalid", () => {
+            assert.throws(
+                () => board.getPieceAt({ row: -1, column: 0 }),
+                RangeError,
+                /^Row -1, column 0 is an invalid position$/
+            );
+        });
     });
 });
